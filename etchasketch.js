19a86c14b8165aee56c154523dd7a0f982aec2d6
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-02-22 15:43:29 -0600

    got rid of 0

--- etchasketch.js
+++ etchasketch.js
@@ -1,11 +1,10 @@
 let divContainer = document.getElementById("container");
-for (let i = 0; i <= 256; i++) {
+for (let i = 1; i <= 256; i++) {
     let selectedDiv = document.createElement("div");
     divContainer.append(selectedDiv);
 }
-//this is nodelist
 const divList = document.querySelectorAll("#container div");
-//converting using Array.prototype.slice.call
+//converting divs to Array
 const arrayDiv = Array.prototype.slice.call(divList);
 let nthNum = [];
 const module16 = document.getElementsByClassName("divs");
@@ -13,22 +12,15 @@ const module16 = document.getElementsByClassName("divs");
 for (let i = 0; i < arrayDiv.length; i++) {
     arrayDiv[i].classList.add("divs");
     arrayDiv[i].innerHTML = "make this box wider";
-    arrayDiv[i].setAttribute('id', i);
+    arrayDiv[i].setAttribute('id', i +1);
     arrayDiv[i].classList.add(i);
     arrayDiv[i] = parseInt(arrayDiv[i].innerHTML);
     console.log(parseInt(module16[i].id));
-
-    //if (arrayDiv[i] % 16 == 0) {
     if (parseInt(module16[i].id )% 16 == 0) {
-
-        //const breakLine = document.querySelectorAll(".breakLine");
         module16[i].classList.add("breakLine");
         const breakLine = document.querySelectorAll("breakLine");
         const br = document.createElement("br");
-        //trying to add a <br> after every element with a class name of breakline
-        //breakLine[i].after(br);
+        //Add a <br> after every element with a class name of breakline
         module16[i].insertAdjacentElement("afterend",br);
-        //console.log(typeof(breakLine[i]));
-        //console.log(breakLine);
     }
 }
