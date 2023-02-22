073c7c816efb2827d72d87f55912478290b7693c
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-02-22 14:22:55 -0600

    got 16 x 16 grid working

--- etchasketch.js
+++ etchasketch.js
@@ -8,39 +8,23 @@ const divList = document.querySelectorAll("#container div");
 //converting using Array.prototype.slice.call
 const arrayDiv = Array.prototype.slice.call(divList);
 let nthNum = [];
-let br = document.createElement("br");
 const module16 = document.getElementsByClassName("divs");
-const breakLine = document.getElementsByClassName("breakLine");
 
 for (let i = 0; i < arrayDiv.length; i++) {
     arrayDiv[i].classList.add("divs");
     arrayDiv[i].innerHTML = i;
     arrayDiv[i].setAttribute('id', i);
-    arrayDiv[i].classList.add(i);
+    //arrayDiv[i].classList.add(i);
     arrayDiv[i] = parseInt(arrayDiv[i].innerHTML);
     if (arrayDiv[i] % 16 == 0) {
-        console.log(arrayDiv[i] + " should equal " + typeof(module16[i].innerHTML));
-        console.log(typeof(module16[16]));
-       // insertAfter(br, )
-       module16[i].classList.add("breakLine");
+        //const breakLine = document.querySelectorAll(".breakLine");
+        module16[i].classList.add("breakLine");
+        const breakLine = document.querySelectorAll("breakLine");
+        const br = document.createElement("br");
+        //trying to add a <br> after every element with a class name of breakline
+        //breakLine[i].after(br);
+        module16[i].insertAdjacentElement("afterend",br);
+        //console.log(typeof(breakLine[i]));
+        //console.log(breakLine);
     }
 }
-
-//make divs 16 x 16
-
-//just in case below backup
-/*
-for (let i = 0; i < arrayDiv.length; i++) {
-    arrayDiv[i].classList.add("divs");
-    arrayDiv[i].innerHTML = i;
-    //arrayDiv[i].toString();
-    arrayDiv[i] = parseInt(arrayDiv[i]);
-    //console.log(typeof(arrayDiv[i]));
-    //console.log(parseInt(divList[i].innerHTML));
-}
-if (arrayDiv[i] % 16 == 0) {
-        console.log(arrayDiv[i]);
-        br.after(".divs");
-    }
-/*/
-    
