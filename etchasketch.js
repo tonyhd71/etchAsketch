7d531dde05941fc85d0efff4e807ce96723c35fa
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-03-02 13:30:56 -0600

    fixed width when resizing etchasketch

--- etchasketch.js
+++ etchasketch.js
@@ -3,36 +3,53 @@ const eachDiv = document.getElementsByClassName("divs");
 console.log(eachDiv);
 let num = 16;
 let inputSquareAmount;
+let divWidth;
+let divHeight;
 fillContainer();
 function fillContainer() {
     for (let i = 1; i <= num*num; i++) { 
         let selectedDiv = document.createElement("div");
         divContainer.append(selectedDiv);
+        divWidth = 960/num;
+        divWidth = divWidth.toString();
+        divWidth = divWidth + 'px';
+        divHeight = divWidth;
+        //console.log(divWidth);
     }
     const divList = document.querySelectorAll("#container div");
     //converting divs to Array
     const arrayDiv = Array.prototype.slice.call(divList);
+    //arrayDiv.style.width = divWidth;
     const module16 = document.getElementsByClassName("divs");
     for (let i = 0; i < arrayDiv.length; i++) {
         arrayDiv[i].classList.add("divs");
         eachDiv[i].style.color = "white";
         arrayDiv[i].innerHTML = '.';
+
+        arrayDiv[i].style.width = divWidth;
+        arrayDiv[i].style.height = divHeight;
+
         arrayDiv[i].setAttribute('id', i +1);
         arrayDiv[i].classList.add(i);
+        //arrayDiv[i].style.width = divWidth;
+        arrayDiv[i] = parseInt(arrayDiv[i].innerHTML);
         arrayDiv[i] = parseInt(arrayDiv[i].innerHTML);
         eachDiv[i].addEventListener("mouseleave", function (e) {
             this.style.backgroundColor = "red";
             this.style.color = "red";
-    });
-        if (parseInt(module16[i].id )% num == 0) {
-            module16[i].classList.add("breakLine");
-            const br = document.createElement("nobr");
-            //Add a <br> after every element with a class name of breakline
-            module16[i].insertAdjacentElement("afterend",br);
+        });
+    if (parseInt(module16[i].id )% num == 0) {
+        module16[i].classList.add("breakLine");
+        const br = document.createElement("nobr");
+        //Add a <br> after every element with a class name of breakline
+        module16[i].insertAdjacentElement("afterend",br);
+    }
     }
 }
+
+
 let clearButton = document.querySelector("button");
-clearButton.addEventListener("mouseup", numSquares);
+clearButton.addEventListener("click", numSquares);
 function numSquares() {
     inputSquareAmount = prompt("how many squares do you want?", num);
     inputSquareAmount = parseInt(inputSquareAmount);
@@ -42,5 +59,4 @@ function numSquares() {
     divContainer.innerHTML = "";
     num = inputSquareAmount;
     fillContainer();
-}
 }
